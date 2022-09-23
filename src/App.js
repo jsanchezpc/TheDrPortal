f6576a4dcc6eaad78f51af13dab7d165ee43318a
Jorge Sánchez <48210515+jsanchezpc@users.noreply.github.com>
2022-09-23 05:53:43 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./css/normalize.css";
+import "./css/skeleton.css";
+import "./css/styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
